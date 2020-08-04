124ffd8f088381dce6c8541c6a0e90ee93bcac17
Florian Proksch <florian.proksch@protonmail.com>
2020-08-04 18:46:17 +0200

    more interactions, help command and switch to my tenorjs fork

--- server.js
+++ server.js
@@ -13,55 +13,146 @@ const client = new Discord.Client()
 
 client.on('ready', () => console.log('ready'))
 
+const commands = {
+  help: {
+    description: 'Shows this message',
+    handler: processHelpRequest,
+  },
+  hug: {
+    usage: '+hug {@mention}',
+    handler: processHugRequest
+  },
+  cuddle: {
+    alias: 'hug'
+  },
+  lick: {
+    usage: '+lick {@mention}',
+    handler: processLickRequest
+  },
+  poke: {
+    usage: '+poke {@mention}',
+    handler: processPokeRequest
+  },
+  touch: {
+    alias: 'poke'
+  },
+  tickle: {
+    usage: '+tickle {@mention}',
+    handler: processTickleRequest
+  },
+  squish: {
+    usage: '+squish {@mention}',
+    handler: processSquishRequest
+  }
+}
+
 const COMMAND = /^\+(?<command>\w+)(?<parameters>.*)/
-const PARAMETERS = /(?:[^\s"]+|"[^"]*")+/g
+const PARAMETERS = /(?:[^\s"]+|"[^"]*")+/
 const MENTION = /<@(?<id>.*)>/
 client.on('message', msg => {
   console.log(msg)
   const match = COMMAND.exec(msg.content)
   if (match) {
-    const parameters = PARAMETERS.exec(match.groups.parameters)
-    switch (match.groups.command) {
-      case 'hug':
-        return processHugRequest(msg, parameters)
-      case 'lick':
-        return processLickRequest(msg, parameters
-        )
-    }
+    let command = commands[match.groups.command]
+    if (!command) return msg.reply('this are not the droids you are looking for')
+
+    const parameters = match.groups.parameters.match(PARAMETERS)
+    if (command.alias) command = commands[command.alias]
+    return command.handler(msg, parameters)
   }
 })
 
-client.login(keys.discord)
+client.login(keys.discord).catch(console.error)
 
-async function processHugRequest(msg, parameters) {
+function processHelpRequest(msg, parameters) {
+  const mergedCommands = Object.entries(commands).reduce((acc, [k, v]) => {
+    if (v.alias) {
+      if (!acc[v.alias]) acc[v.alias] = {aliases: [k]}
+      else acc[v.alias].aliases.push(k)
+    }
+    else acc[k] = {aliases: [], ...acc[k], ...v}
+    return acc
+  },{})
+
+  const helpText = Object.entries(mergedCommands).reduce((acc, [k, v]) => {
+    const usage = v.usage || `+${k}`
+    return acc +
+      usage.padEnd(30, ' ') +
+      (v.description || '') +
+      (v.aliases.length ? `(aliases: ${v.aliases.join(', ')})` : '') +
+      '\n'
+  }, '')
+
+  msg.reply(`this is what i can do for you, master:\n\`\`\`${helpText}\`\`\``)
+}
+
+function processHugRequest(msg, parameters) {
+  return interactionWithRandomGif(msg, parameters, {
+    gifQuery: 'hug',
+    messageTemplate: (author, target) => `**${author}** is hugging **${target}**`,
+    onInvalidParameters: () => msg.channel.send(`<@${msg.author.id}> is hugging the floor`)
+  })
+}
+
+function processLickRequest(msg, parameters) {
+  return interactionWithRandomGif(msg, parameters, {
+    gifQuery: 'lick',
+    messageTemplate: (author, target) => `**${target}** was licked by **${author}**`,
+    onInvalidParameters: () => msg.channel.send(`<@${msg.author.id}>, you shouldn't lick **that**`)
+  })
+}
+
+function processPokeRequest(msg, parameters) {
+  return interactionWithRandomGif(msg, parameters, {
+    gifQuery: 'poke',
+    messageTemplate: (author, target) => `**${target}** was poked by **${author}**`,
+    onInvalidParameters: () => msg.channel.send(`<@${msg.author.id}> is poking his nose`)
+  })
+}
+
+function processSlapRequest(msg, parameters) {
+  return interactionWithRandomGif(msg, parameters, {
+    gifQuery: 'slap',
+    messageTemplate: (author, target) => `**${target}** was slapped by **${author}**`,
+    onInvalidParameters: () => msg.channel.send(`<@${msg.author.id}> slapped itself in it's confusion`)
+  })
+}
+
+function processTickleRequest(msg, parameters) {
+  return interactionWithRandomGif(msg, parameters, {
+    gifQuery: 'tickle',
+    messageTemplate: (author, target) => `**${target}** was tickled by **${author}**`,
+    onInvalidParameters: () => msg.channel.send(`<@${msg.author.id}> was tickled by a tick... ew`)
+  })
+}
+
+function processSquishRequest(msg, parameters) {
+  return interactionWithRandomGif(msg, parameters, {
+    gifQuery: 'squish cheeks',
+    messageTemplate: (author, target) => `**${author}** squishes **${target}'s** cheeks!`,
+    onInvalidParameters: () => msg.channel.send(`<@${msg.author.id}> squishes me! >////<`)
+  })
+}
+
+async function interactionWithRandomGif(msg, parameters, { gifQuery, messageTemplate, onInvalidParameters, limit }) {
   if (!parameters)
-    return msg.channel.send(`<@${msg.author.id}> is hugging the floor`)
+    return onInvalidParameters()
 
   const userIdMatch = MENTION.exec(parameters[0])
   if (!userIdMatch)
     return msg.channel.send(`<@${msg.author.id}> is very confused`)
 
-  const gif = await randomPicture('anime hug')
+  const gif = await randomAnimeTenorPicture(gifQuery, limit)
   sendResponse(msg,
-    `**${msg.author.username}** is hugging **${msg.mentions.users.get(userIdMatch.groups.id).username}**`,
+    messageTemplate(msg.author.username, msg.mentions.users.get(userIdMatch.groups.id).username),
     gif.media[0].gif.url)
 }
 
-async function processLickRequest(msg, parameters) {
-  if (!parameters)
-    return msg.channel.send(`<@${msg.author.id}>, you shouldn't lick **that**`)
-
-  const userIdMatch = MENTION.exec(parameters[0])
-  if (!userIdMatch)
-    return msg.channel.send(`<@${msg.author.id}> is very confused`)
-
-  const gif = await randomPicture('anime lick')
-  sendResponse(msg,
-    `**${msg.mentions.users.get(userIdMatch.groups.id).username}** was licked by **${msg.author.username}**`,
-    gif.media[0].gif.url)
+function randomAnimeTenorPicture(query, limit = 30) {
+  return randomTenorPicture(`anime ${query}`, limit)
 }
 
-async function randomPicture(query, limit = 10) {
+async function randomTenorPicture(query, limit) {
   let pick = Math.ceil(Math.random() * limit) - 1
 
   const gifs = await tenor.Search.Query(query, limit)
