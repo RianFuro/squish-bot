e236b9abd566123a144e681afdc8f2e990057051
Florian Proksch <florian.proksch@protonmail.com>
2020-08-07 17:08:36 +0200

    Add cookie and snack command

--- server.js
+++ server.js
@@ -68,6 +68,11 @@ const commands = {
     usage: '+star {@mention}',
     handler: processStarRequest
   },
+  snack: {
+    usage: '+snack [healthy] [<amount>]',
+    handler: processSnackRequest
+  },
+  snacc: {alias: 'snack'},
   nom: {alias: 'bite'},
   '8ball': {
     usage: '+8ball {question}',
@@ -306,6 +311,42 @@ function processLaughRequest(msg) {
 
 function processWagRequest(msg) {
   return animeGifResponse(msg, { gifQuery: 'wag tail' })
+}
+
+const snacks = require('./lists/snacks.json')
+function processSnackRequest(msg, parameters) {
+  let kind, amount
+  const pOne = parameters.shift()
+  if (!pOne) {
+    kind = 'all'
+    amount = 1
+  } else if (pOne === 'healthy') {
+    kind = 'healthy'
+    const pTwo = parameters.shift()
+    if (!pTwo) amount = 1
+    else if (!isNaN(pTwo)) amount = pTwo | 0
+    else return msg.channel.send(`<@${msg.author.id}> is very confused`)
+  } else if (!isNaN(pOne)) {
+    kind = 'all'
+    amount = pOne | 0
+  } else return msg.channel.send("I'm not sure I've got something like that (╯_╰)")
+
+  if (amount > 50) return msg.reply("b-but, that's way too many, master! ∑(O_O;)")
+
+  let snackList
+    if (kind === 'all')
+     snackList = Object.values(snacks).flat()
+    else if (kind === 'healthy')
+     snackList = snacks.healthy
+    else snackList = []
+
+  const chosenSnacks = []
+  for (let i = 0; i < amount; i++) {
+    let pick = Math.ceil(Math.random() * snackList.length) - 1
+    chosenSnacks.push(snackList[pick])
+  }
+
+  msg.reply('here you go, master! (o´▽`o)\n' + chosenSnacks.join(''))
 }
 
 async function interactionWithRandomGif(msg, parameters, { gifQuery, messageTemplate, onInvalidParameters, limit, imageLoader }) {
