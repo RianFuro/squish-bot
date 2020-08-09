beed0bf37aba7843518f5f74db20761f531b3e30
Florian Proksch <florian.proksch@protonmail.com>
2020-08-09 20:18:53 +0200

    fix workout command

--- commands/workout.js
+++ commands/workout.js
@@ -2,7 +2,7 @@ const {randomAnimeGifResponse, gifFromListResponse} = require('../responses')
 module.exports = {
   usage: '+workout [random]',
   handler(msg, parameters) {
-    if (['r', 'rand', 'random']) return randomAnimeGifResponse(msg, {gifQuery: 'workout'})
+    if (['r', 'rand', 'random'].includes(parameters[0])) return randomAnimeGifResponse(msg, {gifQuery: 'workout'})
     else return gifFromListResponse(msg, { gifList: require('../lists/workout.json') })
   }
 }
